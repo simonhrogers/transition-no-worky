81fd5feea7cca8c25fa94102249075a17c7d79e2
Simon Rogers <hello@simonrogers.info>
2024-09-18 16:07:34 +0100

    lazysizes minimal repro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
     pageTransition: { name: 'fade' }, 
     layoutTransition: { name: 'layout' },
   },
+  plugins: [
+    '@/plugins/lazysizes.client.js',
+  ],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
