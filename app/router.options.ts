e239b969f10e9cc2ebdae8df0cbfbbbfa00b320d
Simon Rogers <hello@simonrogers.info>
2024-09-19 15:48:33 +0100

    Valid

---
+++ app/router.options.ts
@@ -0,0 +1,15 @@
+import type { RouterOptions } from '@nuxt/schema'
+
+export default <RouterOptions>{
+  // scrollBehavior (to, from, savedPosition) {
+  //   const nuxtApp = useNuxtApp()
+
+    
+  //   return new Promise((resolve) => {
+  //     nuxtApp.hooks.hookOnce('page:finish', async () => {
+  //       await nextTick()
+  //       resolve(savedPosition || { top: 0 })
+  //     })
+  //   })
+  // }
+}
