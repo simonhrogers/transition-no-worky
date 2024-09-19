fdc1339d9a4e9b443e9d9d713a1245450e2b86d1
Simon Rogers <hello@simonrogers.info>
2024-09-19 15:52:29 +0100

    demonstrate router options doesn’t work

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,15 +1,15 @@
 import type { RouterOptions } from '@nuxt/schema'
 
 export default <RouterOptions>{
-  // scrollBehavior (to, from, savedPosition) {
-  //   const nuxtApp = useNuxtApp()
+  scrollBehavior (to, from, savedPosition) {
+    const nuxtApp = useNuxtApp()
 
     
-  //   return new Promise((resolve) => {
-  //     nuxtApp.hooks.hookOnce('page:finish', async () => {
-  //       await nextTick()
-  //       resolve(savedPosition || { top: 0 })
-  //     })
-  //   })
-  // }
+    return new Promise((resolve) => {
+      nuxtApp.hooks.hookOnce('page:finish', async () => {
+        await nextTick()
+        resolve(savedPosition || { top: 0 })
+      })
+    })
+  }
 }
